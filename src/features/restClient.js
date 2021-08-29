e8b216cda55d01ad5b5f599be0feb832eb556e25
Rockie Guo <rockie@kitmi.com.au>
2021-08-30 02:52:03 +1000

    Fix rest client result.

--- src/features/restClient.js
+++ src/features/restClient.js
@@ -99,7 +99,7 @@ class RestClient {
 
         try {
             const res = await req;
-            const result = ((!res.body || res.body === "") && res.text !== "") ? res.text : res.body;
+            const result = res.type === 'text/plain' ? res.text : res.body;
 
             if (this.onSent) {
                 await this.onSent(url, result);
